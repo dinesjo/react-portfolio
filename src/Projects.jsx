dd664471a4d0d4d5fa8186afc7aa6f17b3b678e6
dinesjo <dinesjo@kth.se>
2023-09-05 18:09:53 +0200

    Add projects and add color-map

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,43 @@
-import { FaCertificate, FaLink, FaRoad, FaServer, FaUnity } from "react-icons/fa";
+import {
+  FaCertificate,
+  FaChess,
+  FaChessBoard,
+  FaGithub,
+  FaLink,
+  FaMapPin,
+  FaMusic,
+  FaRoad,
+  FaServer,
+  FaUnity,
+} from "react-icons/fa";
 
-const ProjectCard = ({ children, title, imgsrc, hrefText, href, tags, icon }) => {
+const ProjectCard = ({
+  children,
+  title,
+  imgsrc,
+  hrefText,
+  href,
+  tags,
+  icon,
+  githubhref,
+}) => {
+  const colorMap = {
+    "c++": "teal",
+    wxwidgets: "lime",
+    "c#": "green",
+    javascript: "indigo",
+    kql: "red",
+    "asp.net": "orange",
+    html: "blue",
+    css: "pink",
+    react: "cyan",
+    "tailwind css": "emerald",
+    bootstrap: "purple",
+    python: "amber",
+    "twitter api": "fuchsia",
+    unity: "lime",
+  };
+
   return (
     <div
       className="bg-slate-700 rounded-lg shadow-md overflow-hidden"
@@ -11,32 +48,63 @@ const ProjectCard = ({ children, title, imgsrc, hrefText, href, tags, icon }) =>
         e.currentTarget.querySelector("img").style.transform = "scale(1)";
       }}
     >
+      {/* IMAGE */}
       <img
         src={imgsrc}
         alt={title}
-        className="w-full h-32 sm:h-48 object-cover transition-all duration-300 overflow-hidden"
+        className={`w-full h-32 sm:h-48 object-cover transition-all duration-300 overflow-hidden ${
+          href && "cursor-pointer"
+        }`}
+        onClick={() => {
+          if (href) window.open(href, "_blank");
+        }}
       />
+
       <div className="p-6">
-        <div className="flex gap-2 mb-3">
+        {/* TAGS */}
+        <div className="flex gap-2 mb-3 flex-wrap">
           {tags.map((tag) => (
             <span
-              key={tag.content}
-              className={`text-sm text-white bg-${tag.color}-700 opacity-80 px-2 py-1 rounded-full`}
+              key={tag}
+              className={`text-sm text-gray-200 bg-${
+                colorMap[tag.toLowerCase()]
+              }-700 bg-opacity-60 px-2 py-1 rounded-full whitespace-nowrap`}
             >
-              {tag.content}
+              {tag}
             </span>
           ))}
         </div>
-        <h1 className="text-xl font-semibold mb-2">{icon}{title}</h1>
+
+        {/* TITLE */}
+        <h1 className="text-xl font-semibold mb-2">
+          {icon}
+          {title}
+        </h1>
+
+        {/* DESCRIPTION */}
         <p className="text-gray-600 dark:text-gray-400 mb-3">{children}</p>
+
+        {/* LINK */}
         {href && (
           <a
             href={href}
             target="_blank"
-            className="rounded text-neutral-50 bg-indigo-500 hover:bg-indigo-600 px-3 py-1 mt-3 inline-block transition-all"
+            className="rounded-lg text-neutral-50 bg-indigo-500 hover:bg-indigo-600 px-3 py-1 mt-3 inline-block transition-all"
           >
             <FaLink className="inline-block me-2" />
             {hrefText}
+          </a>
+        )}
+
+        {/* GITHUB LINK */}
+        {githubhref && (
+          <a
+            href={githubhref}
+            target="_blank"
+            title="View on GitHub"
+            className="px-3 py-1 float-right mb-4"
+          >
+            <FaGithub className="inline-block text-4xl hover:text-gray-400 transition-all" />
           </a>
         )}
       </div>
@@ -48,29 +116,31 @@ const Projects = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center pt-20">
+        {/* TITLE */}
         <h1 className="text-6xl font-semibold flex justify-center mb-10">
           <FaCertificate className="inline-block me-5" />
           Projects
         </h1>
-        <h4 className="text-2xl mb-10">
+
+        {/* SUBTITLE */}
+        <h4 className="text-2xl mb-10 w-3/4 text-center">
           Here are some of the projects I have worked on, and which{" "}
-          <span className="text-lg text-white bg-gray-700 opacity-80 px-2 py-1 rounded-full">
+          <span className="text-lg text-gray-200 bg-slate-500 bg-opacity-60 px-2 py-1 rounded-full">
             technologies
           </span>{" "}
           were used.
         </h4>
+
         {/* Grid of cards, each one having an image, header and description */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-14 w-2/3">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-14 w-2/3 mb-5">
           <ProjectCard
             title="3D drone relief software"
             imgsrc="src\assets\drone-software.png"
             href="https://i-conicvision.com/2022/12/15/kth-selected-proposal-from-i-conic-again/"
             hrefText="Read I-CONIC blog"
-            tags={[
-              { content: "C++", color: "teal" },
-              { content: "wxWidgets", color: "lime" },
-            ]}
-            icon={<FaUnity className="inline-block me-2" />}
+            githubhref={"https://github.com/I-CONIC-Vision-AB/iconic-measure"}
+            tags={["C++", "wxWidgets"]}
+            icon={<FaUnity className="inline-block me-2 text-lime-600" />}
           >
             In a collaborative effort with seven other KTH students, we
             developed a user-friendly interface for an existing 3D drone
@@ -90,12 +160,15 @@ const Projects = () => {
             title="Log Portal"
             imgsrc="src\assets\log-portal.png"
             tags={[
-              { content: "C#", color: "green" },
-              { content: "JavaScript", color: "indigo" },
-              { content: "KQL", color: "red" },
-              { content: "ASP.net", color: "orange" },
+              "C#",
+              "KQL",
+              "ASP.NET",
+              "JavaScript",
+              "Bootstrap",
+              "HTML",
+              "CSS",
             ]}
-            icon={<FaServer className="inline-block me-2" />}
+            icon={<FaServer className="inline-block me-2 text-emerald-500" />}
           >
             I provided{" "}
             <a
@@ -115,17 +188,65 @@ const Projects = () => {
             imgsrc="src\assets\path-vis.png"
             href="https://linusdinesjo.github.io/pathfinding-vis/"
             hrefText="Try Yourself"
-            tags={[
-              { content: "JavaScript", color: "indigo" },
-              { content: "HTML", color: "blue" },
-              { content: "CSS", color: "pink" },
-            ]}
-            icon={<FaRoad className="inline-block me-2" />}
+            githubhref={"https://github.com/linusdinesjo/pathfinding-vis"}
+            tags={["JavaScript", "HTML", "CSS"]}
+            icon={<FaRoad className="inline-block me-2 text-blue-500" />}
           >
             Upper secondary school graduate project (Gymnasie&shy;arbete) where
             I developed a pathfinding visualization website. The project aimed
             to study the most popular pathfinding algorithms and show their
             characteristics in a visual form.
+          </ProjectCard>
+          <ProjectCard
+            title="This Website"
+            imgsrc="src\assets\this.png"
+            githubhref="https://github.com/linusdinesjo/react-portfolio"
+            tags={["React", "Tailwind CSS", "JavaScript", "HTML", "CSS"]}
+            icon={<FaMapPin className="inline-block text-red-500 me-2" />}
+          >
+            This portfolio website is my first project using React and Tailwind
+            CSS. I wanted to learn these technologies and decided to make a
+            portfolio website to showcase my projects.
+          </ProjectCard>
+          <ProjectCard
+            title="Chess Reporter"
+            imgsrc="src\assets\chess-reporter.png"
+            href="https://twitter.com/ChessReporter/"
+            hrefText="View on X (Formerly Twitter)"
+            githubhref="https://gits-15.sys.kth.se/wver/projinda-twitter-bot"
+            tags={["Python", "Twitter API"]}
+            icon={<FaChess className="inline-block me-2 text-amber-700" />}
+          >
+            A Twitter bot that posts Python-generated GIFs of high-profile chess
+            games. The project was part of the course{" "}
+            <a
+              href="https://www.kth.se/student/kurser/kurs/DD1349?periods=6&startterm=20221&l=en"
+              target="_blank"
+              className="link"
+            >
+              DD1349
+            </a>
+            .
+          </ProjectCard>
+          <ProjectCard
+            title="Note Hero"
+            imgsrc="src\assets\note-hero.png"
+            tags={["Unity", "C#"]}
+            icon={<FaMusic className="inline-block me-2 text-purple-500" />}
+          >
+            A rhythm game inspired by Guitar Hero. After interviewing music
+            teachers I, along with other students, developed a game to help
+            gamify music theory. The game used a mobile device's microphone to
+            detect a players real-life instrument. The project was part of the
+            course{" "}
+            <a
+              href="https://www.kth.se/student/kurser/kurs/DH1620?periods=6&startterm=20221&l=en"
+              target="_blank"
+              className="link"
+            >
+              DH1620
+            </a>
+            .
           </ProjectCard>
         </div>
       </div>
