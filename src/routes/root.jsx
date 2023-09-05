65232b49b852a3251e4a47c69a0e658848ff3d0e
dinesjo <dinesjo@kth.se>
2023-09-05 18:09:25 +0200

    Add footer - LinkedIn, Email and GitHub links

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,14 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
-import { Outlet, Link, useLocation } from 'react-router-dom'
-import { FaHome, FaCopy, FaEnvelope, FaCertificate } from 'react-icons/fa'
+import { Fragment, useState } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import { Outlet, Link, useLocation } from "react-router-dom";
+import {
+  FaHome,
+  FaCopy,
+  FaEnvelope,
+  FaCertificate,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const SideBar = () => {
   return (
@@ -10,28 +17,56 @@ const SideBar = () => {
       <SideBarItem name="Projects" icon={<FaCertificate />} to="/projects" />
       <SideBarItem name="Contact" icon={<FaEnvelope />} to="/contact" />
     </div>
-  )
-}
+  );
+};
 
 const SideBarItem = ({ name, icon, to }) => {
   let current = useLocation().pathname === to;
   return (
     <Link
       to={to}
-      className={`flex items-center p-2 text-lg font-semibold rounded-md transition-all ${current ? 'text-indigo-500' : 'text-zinc-300 hover:text-indigo-500'}`}
-      onClick={() => {console.log(current)}}
+      className={`flex items-center p-2 text-lg font-semibold rounded-md transition-all ${
+        current ? "text-indigo-500" : "text-zinc-300 hover:text-indigo-500"
+      }`}
     >
-      <span className="text-xl">{icon}</span>
-      <span className="ml-2">{name}</span>
+      <span className="inline-block me-2">{icon}</span>
+      {name}
     </Link>
-  )
-}
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center gap-10 mt-auto py-5 text-gray-300 bg-neutral-800">
+      <a
+        href="https://github.com/linusdinesjo"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="View my GitHub"
+      >
+        <FaGithub className="text-4xl hover:text-gray-400 transition-all" />
+      </a>
+      <a href={`mailto:dinesjo@kth.se`} title="Send me an email">
+        <FaEnvelope className="text-4xl hover:text-gray-400 transition-all" />
+      </a>
+      <a
+        href="http://linkedin.com/in/linus-dinesjö-6b1b3b1b0"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Open my LinkedIn"
+      >
+        <FaLinkedin className="text-4xl hover:text-gray-400 transition-all" />
+      </a>
+    </div>
+  );
+};
 
 export default function Root() {
   return (
     <div className="flex flex-col min-h-screen">
       <SideBar />
       <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
