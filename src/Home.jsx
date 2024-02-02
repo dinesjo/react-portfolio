7c3fe954e071ea020637b00cdcb16d96dd98c950
Linus Dinesjö <dinesjo@kth.se>
2024-02-02 20:42:48 +0100

    add portrait to home-page

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,10 +7,26 @@ export default function Home() {
   return (
     <>
       {/* Using tailwind CSS, write a simple portfolio website */}
-      <div className="flex flex-col justify-center h-screen">
-        <h1 className="h1 mb-3 text-indigo-500 font-serif">Linus Dinesjö</h1>
+      <div className="flex flex-col justify-center h-screen pt-28">
+        <div className="flex justify-center relative mb-3">
+          <img
+            src="/public/portrait.png"
+            alt=""
+            className="w-60 xs:w-72 rounded-full mx-auto absolute bottom-0 -z-50"
+          />
+          <h1
+            className="h1 text-indigo-500 font-serif underline underline-offset-4"
+            style={{
+              // text shadow
+              textShadow: "2px 2px 4px #000000",
+            }}
+          >
+            Linus Dinesjö
+          </h1>
+        </div>
         <h4 className="h4">
-          <b>Hi!</b> Please check out some of my{" "}
+          <b className="text-indigo-500">Welcome!</b> Please check out some of
+          my{" "}
           <Link className="link" to="/projects">
             projects
           </Link>
@@ -25,8 +41,8 @@ export default function Home() {
           page.
         </h4>
         <h5 className="h5 mt-5 text-gray-400">
-          I&apos;m a <em>third year</em> student at <b>KTH</b> Stockholm. Persuing a{" "}
-          <b>master&apos;s degree in Computer Science</b>.
+          I&apos;m a <em>third year</em> student at <b>KTH</b> Stockholm.
+          Persuing a <b>master&apos;s degree in Computer Science</b>.
         </h5>
       </div>
     </>
